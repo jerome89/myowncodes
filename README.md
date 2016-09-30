9dc320993617e6d66b6a1ee13fb7a438ec43c277
jaerom89@gmail.com <jaerom89@gmail.com>
2016-10-01 01:00:11 +0900

    README

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ichker.py
-## Internet connection checker written in python
+## Internet connection checker, written in python
 ### 1. Set your own ethernet interface & log file path in ichker.py
 ### 2. Please make sure that you have sudo privilege in terminal(type 'sudo -s' in terminal).
 ### 3. Run the code, type 'python ichker.py' in terminal
--- ichker.py
+++ ichker.py
@@ -6,26 +6,27 @@ from datetime import datetime
 from subprocess import call
 
 url = 'https://www.google.com' # Set desired page to test connection
-device = 'YOUR_ETHERNET_INTERFACE' # Change it to your ethernet interface, can be shown when you type 'ifconfig' in terminal
+device = 'YOUR_ETHERNET_INTERFACE' # Change this parameter to your ethernet interface, can be shown when you type 'ifconfig' in terminal
+log_file_path = '/YOUR_DESIRED_PATH/DESIRED_FILE_NAME.txt' # Set your log file path here...
 
-def ichker():
-    fail_count = 0 # Initialize failure count by 0
+def ichker(chk_interval, log):
+    drop_count = 0 # Initialize drop count by 0
     while True:
-        for i in xrange(300, 0, -1): # For 300 seconds waiting...to prevent annoying server
+        for i in xrange(chk_interval, 0, -1): # 'chk_interval' seconds for waiting...to prevent annoying server
             time.sleep(1)
             sys.stdout.write('\r')
             sys.stdout.write('%03d' % (i))
             sys.stdout.flush()
         try:            
-            urllib.urlopen(url)
+            urllib.urlopen(url) # Connection check
         except:
-            print '' # Make log every time the connection fails...
-            log_file = open('/YOUR_DESIRED_PATH/DESIRED_FILE_NAME.txt', 'a+') # Change the log file path to your own
-            fail_count += 1
+            print '' # Make log every time the connection drops...
+            log_file = open(log, 'a+')
+            drop_count += 1
             dateandtime = datetime.today().strftime('%Y/%m/%d %H:%M:%S') # Get exact time and date
-            print 'Connection failed!! Restarting network...'
-            print 'Fail count : ' + str(fail_count)
-            to_write = 'Connection failed at ' + dateandtime + '\n'
+            print 'Connection dropped!! Restarting network...'
+            print 'Drop count : ' + str(drop_count)
+            to_write = 'Connection dropped at ' + dateandtime + '\n'
             log_file.write(to_write)
             if_down = 'ifconfig ' + device + ' down' # eth down(disconnect) command
             if_up = 'ifconfig ' + device + ' up' # eth up(reconnect) command
@@ -34,4 +35,4 @@ def ichker():
             log_file.close()
 
 print 'Connection checking has started...'
-ichker()
+ichker(300, log_file_path)
